0b443a4ae3f3f85027f7d390ff9ff977af0cba42
Dicki <radicmrs51@gmail.com>
2020-10-12 08:51:35 +0700

    Berhasil membuat garis segitiga dengan gl.TRIANGLE_FAN

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ function main() {
     var vertices = [-0.5, 0.5, // A Point
         -0.5, -0.5, // B point
         0.5, -0.5, // C Point
-        -0.5, 0.5
     ];
 
     var positionBuffer = gl.createBuffer();
@@ -52,9 +51,9 @@ function main() {
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    var primitive = gl.LINE_STRIP;
+    var primitive = gl.TRIANGLE_FAN;
     var offset = 0;
-    var count = 4; // Jumlah vertex yang akan digambar
+    var count = 3; // Jumlah vertex yang akan digambar
     gl.drawArrays(primitive, offset, count)
     gl.drawArrays(gl.POINTS, 0, 1)
 }
