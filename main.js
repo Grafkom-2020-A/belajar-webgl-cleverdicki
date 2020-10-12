b011020ad19826862f95cc35a2b84041093b2f98
Dicki <radicmrs51@gmail.com>
2020-10-12 08:35:41 +0700

    Berhasil membuat tiga titik untuk segitiga

--- main.js
+++ main.js
@@ -2,9 +2,24 @@ function main() {
     var canvas = document.getElementById("myCanvas");
     var gl = canvas.getContext("webgl");
 
+    // Define vertices of triangle
+    /*
+        A (-0.5, 0.5); B (-0.5, -0.5); C (0.5, -0.5)
+    */
+    var vertices = [-0.5, 0.5, // A Point
+        -0.5, -0.5, // B point
+        0.5, -0.5 // C Point
+    ];
+
+    var positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
     var vertexShaderCode = `
+    attribute vec2 a_Position;
     void main() {
-        gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+        gl_Position = vec4(a_Position, 0.0, 1.0);
         gl_PointSize = 10.0;
     }
     `;
@@ -15,7 +30,7 @@ function main() {
 
     var fragmentShaderCode = `
     void main() {
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
     }
     `;
 
@@ -29,8 +44,17 @@ function main() {
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
 
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    var aPosition = gl.getAttribLocation(shaderProgram, "a_Position");
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(aPosition);
+
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
+    var primitive = gl.POINTS;
+    var offset = 0;
+    var count = 3; // Jumlah vertex yang akan digambar
+    gl.drawArrays(primitive, offset, count)
     gl.drawArrays(gl.POINTS, 0, 1)
 }
