2f0412dbe688bfd57984816c3bf8b5286c500991
Clever Dicki <48941105+cleverdicki@users.noreply.github.com>
2020-11-02 15:37:03 +0700

    Berhasil translasi x dan y dengan matrix

--- main.js
+++ main.js
@@ -2,37 +2,30 @@ function main() {
     var canvas = document.getElementById("myCanvas");
     var gl = canvas.getContext("webgl");
 
-    // Define vertices of triangle
-    /*
-        A (-0.5, 0.5); B (-0.5, -0.5); C (0.5, -0.5); D (0.5, 0.5)
-    */
-    var vertices = [-0.5, 0.5, // A Point
-        -0.5, -0.5, // B point
-        0.5, -0.5, // C Point
-        0.5, 0.5 // D Point
+    // Definisi verteks-verteks pada segitiga
+    /**
+     * A (-0.5, 0.5); B (-0.5, -0.5); C (0.5, -0.5); D (0.5, 0.5)
+     */
+    var vertices = [-0.5, 0.5, 1.0, 0.0, 0.0, // Titik A 
+        -0.5, -0.5, 1.0, 0.0, 0.0, // Titik B
+        0.5, -0.5, 1.0, 0.0, 0.0, // Titik C
+        0.5, -0.5, 0.0, 0.0, 1.0, // Titik C
+        0.5, 0.5, 0.0, 0.0, 1.0, // Titik D
+        -0.5, 0.5, 0.0, 0.0, 1.0 // Titik A 
     ];
 
-    var positionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    var vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
-    var vertexShaderCode = `
-    attribute vec2 a_Position;
-    void main() {
-        gl_Position = vec4(a_Position, 0.0, 1.0);
-    }
-    `;
+    var vertexShaderCode = document.getElementById("vertexShaderCode").text;
 
     var vertexShader = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShader, vertexShaderCode);
     gl.compileShader(vertexShader);
 
-    var fragmentShaderCode = `
-    void main() {
-        gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
-    }
-    `;
+    var fragmentShaderCode = document.getElementById("fragmentShaderCode").text;
 
     var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragmentShader, fragmentShaderCode);
@@ -44,18 +37,49 @@ function main() {
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     var aPosition = gl.getAttribLocation(shaderProgram, "a_Position");
-    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(aPosition)
+    var aColor = gl.getAttribLocation(shaderProgram, "a_Color");
+    gl.vertexAttribPointer(
+        aPosition,
+        2,
+        gl.FLOAT,
+        false,
+        5 * Float32Array.BYTES_PER_ELEMENT,
+        0);
+    gl.vertexAttribPointer(
+        aColor,
+        3,
+        gl.FLOAT,
+        false,
+        5 * Float32Array.BYTES_PER_ELEMENT,
+        2 * Float32Array.BYTES_PER_ELEMENT);
+    gl.enableVertexAttribArray(aPosition);
+    gl.enableVertexAttribArray(aColor);
 
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.viewport(100, 0, canvas.width - 200, canvas.height);
+    gl.viewport(100, 0, canvas.height, canvas.height);
 
-    var primitive = gl.TRIANGLE_FAN;
+    var primitive = gl.TRIANGLES;
     var offset = 0;
-    var count = 4; // Jumlah vertex yang akan digambar
-    gl.drawArrays(primitive, offset, count)
-    gl.drawArrays(gl.POINTS, 0, 1)
+    var count = 6; // Jumlah verteks yang akan digambar
+
+    var dx = 0;
+    var dy = 0;
+    var dz = 0;
+    var uDx = gl.getUniformLocation(shaderProgram, 'dx');
+    var uDy = gl.getUniformLocation(shaderProgram, 'dy');
+    var uDz = gl.getUniformLocation(shaderProgram, 'dz');
+    gl.uniform1f(uDz, dz); // Tidak perlu di dalam loop render, karena dz tidak berubah
+
+    function render() {
+        dx += 0.001;
+        dy += 0.001;
+        gl.uniform1f(uDx, dx);
+        gl.uniform1f(uDy, dy);
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.drawArrays(primitive, offset, count);
+        requestAnimationFrame(render);
+    }
+    requestAnimationFrame(render);
 }
