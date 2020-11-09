954430b5525da806e856d2a1b90fb0cca301a304
Clever Dicki <48941105+cleverdicki@users.noreply.github.com>
2020-11-09 14:23:36 +0700

    Menambah fitur freeze

--- main.js
+++ main.js
@@ -2,22 +2,30 @@ function main() {
     var canvas = document.getElementById("myCanvas");
     var gl = canvas.getContext("webgl");
 
-    var vertexShaderCode = `
-    void main() {
-        gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
-        gl_PointSize = 10.0;
-    }
-    `;
+    // Definisi verteks-verteks pada segitiga
+    /**
+     * A (-0.5, 0.5); B (-0.5, -0.5); C (0.5, -0.5); D (0.5, 0.5)
+     */
+    var vertices = [-0.5, 0.5, 1.0, 0.0, 0.0, // Titik A 
+        -0.5, -0.5, 1.0, 0.0, 0.0, // Titik B
+        0.5, -0.5, 1.0, 0.0, 0.0, // Titik C
+        0.5, -0.5, 0.0, 0.0, 1.0, // Titik C
+        0.5, 0.5, 0.0, 0.0, 1.0, // Titik D
+        -0.5, 0.5, 0.0, 0.0, 1.0 // Titik A 
+    ];
+
+    var vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    var vertexShaderCode = document.getElementById("vertexShaderCode").text;
 
     var vertexShader = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShader, vertexShaderCode);
     gl.compileShader(vertexShader);
 
-    var fragmentShaderCode = `
-    void main() {
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-    }
-    `;
+    var fragmentShaderCode = document.getElementById("fragmentShaderCode").text;
 
     var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragmentShader, fragmentShaderCode);
@@ -29,8 +37,73 @@ function main() {
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
 
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    var aPosition = gl.getAttribLocation(shaderProgram, "a_Position");
+    var aColor = gl.getAttribLocation(shaderProgram, "a_Color");
+    gl.vertexAttribPointer(
+        aPosition,
+        2,
+        gl.FLOAT,
+        false,
+        5 * Float32Array.BYTES_PER_ELEMENT,
+        0);
+    gl.vertexAttribPointer(
+        aColor,
+        3,
+        gl.FLOAT,
+        false,
+        5 * Float32Array.BYTES_PER_ELEMENT,
+        2 * Float32Array.BYTES_PER_ELEMENT);
+    gl.enableVertexAttribArray(aPosition);
+    gl.enableVertexAttribArray(aColor);
 
-    gl.drawArrays(gl.POINTS, 0, 1)
+    gl.viewport(100, 0, canvas.height, canvas.height);
+
+    var primitive = gl.TRIANGLES;
+    var offset = 0;
+    var count = 6; // Jumlah verteks yang akan digambar
+
+    var dx = 0;
+    var dy = 0;
+    var dz = 0;
+    var uDx = gl.getUniformLocation(shaderProgram, 'dx');
+    var uDy = gl.getUniformLocation(shaderProgram, 'dy');
+    var uDz = gl.getUniformLocation(shaderProgram, 'dz');
+
+    // Elemenet interaktif
+    var freeze = false;
+
+    function onMouseClick(event) {
+        freeze = !freeze;
+    }
+
+    document.addEventListener('click', onMouseClick, false);
+
+    function onKeyDown(event) {
+        if (event.keyCode == 32) freeze = true;
+    }
+
+    function onKeyUp(event) {
+        if (event.keyCode == 32) freeze = false;
+    }
+
+    document.addEventListener('keydown', onKeyDown, false);
+    document.addEventListener('keyup', onKeyUp, false);
+
+
+    function render() {
+        if (!freeze) {
+            dx += 0.001;
+            dy += 0.001;
+            dz += 0.001;
+        }
+        gl.uniform1f(uDx, dx);
+        gl.uniform1f(uDy, dy);
+        gl.uniform1f(uDz, dz);
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.drawArrays(primitive, offset, count);
+        requestAnimationFrame(render);
+    }
+    requestAnimationFrame(render);
 }
